ab16be538499224722c05544153665df7e1b4288
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-15 11:51:37 +0530

    feat: profile update validations added -> first_name, last_name

--- src/components/auth/profile-validation-schema.tsx
+++ src/components/auth/profile-validation-schema.tsx
@@ -2,6 +2,8 @@ import * as yup from 'yup';
 
 export const profileValidationSchema = yup.object().shape({
   display_name: yup.string().required('form:error-name-required'),
+  first_name: yup.string().required('form:error-first-name-required'),
+  last_name: yup.string().required('form:error-last-name-required'),
   // profile: yup.object().shape({
   //   contact: yup.string().max(19, 'maximum 19 digit').optional(),
   // }),
