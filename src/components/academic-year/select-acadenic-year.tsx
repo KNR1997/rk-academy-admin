88637aaf67d511f16b4c6e553a998e7191fb0658
KNR1997 <kethakaranasinghe@gmail.com>
2026-05-20 23:16:02 +0530

    feat(component): SelectAcademicYear

---
+++ src/components/academic-year/select-acadenic-year.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import { useTranslation } from 'next-i18next';
+import { Control, FieldErrors } from 'react-hook-form';
+// hooks
+import { useAcademicYearsQuery } from '@/data/academic-year';
+// components
+import SelectInput from '@/components/ui/select-input';
+import ValidationError from '@/components/ui/form-validation-error';
+
+export default function SelectAcademicYear({
+  control,
+  errors,
+}: {
+  control: Control<any>;
+  errors: FieldErrors;
+}) {
+  const { locale } = useRouter();
+  const { t } = useTranslation();
+  const { academicYears, loading } = useAcademicYearsQuery({ language: locale });
+  return (
+    <div>
+      <SelectInput
+        label={t('form:input-label-grade-level')}
+        name="grade_level"
+        control={control}
+        getOptionLabel={(option: any) => option.name}
+        getOptionValue={(option: any) => option.id}
+        options={academicYears!}
+        isLoading={loading}
+        required
+      />
+      <ValidationError message={t(errors.academic_year?.message)} />
+    </div>
+  );
+}
