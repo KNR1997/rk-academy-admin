941dd96b529d7dea997882d4fbc42d00a159f8d4
KNR1997 <kethakaranasinghe@gmail.com>
2026-06-09 19:06:43 +0530

    Reapply "feat: handle multiple payment months on Enrollment payment"
    This reverts commit 0395e66c77711332bd800ca8bc937029cd54bc21.

--- src/components/enrollment-payments/enrollment-payment-validation-schema.ts
+++ src/components/enrollment-payments/enrollment-payment-validation-schema.ts
@@ -1,8 +1,26 @@
 import * as yup from 'yup';
 
-export const enrollmentPaymentValidationSchema = yup.object().shape({
+export const enrollmentPaymentValidationSchema = yup.object({
   student: yup.object().required('form:error-student-required'),
+
   enrollment: yup.object().required('form:error-enrollment-required'),
-  payment_month: yup.object().required('form:error-month-required'),
-  fee: yup.string().required('form:error-fee-required'),
+
+  payments: yup
+    .array()
+    .of(
+      yup.object({
+        payment_month: yup
+          .object()
+          .nullable()
+          .required('form:error-month-required'),
+
+        fee: yup
+          .number()
+          .typeError('form:error-fee-required')
+          .required('form:error-fee-required')
+          .positive('form:error-fee-required'),
+      }),
+    )
+    .min(1, 'form:error-month-required')
+    .required(),
 });
