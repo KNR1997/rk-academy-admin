69500a09ee5b48b874f76318ec7c077dbaf3fb17
KNR1997 <kethakaranasinghe@gmail.com>
2026-07-20 20:19:51 +0530

    feat: Icons updated

--- src/components/icons/sidebar/admin.tsx
+++ src/components/icons/sidebar/admin.tsx
@@ -1,11 +1,9 @@
 export const AdminIcon: React.FC<React.SVGAttributes<{}>> = (props) => (
   <svg
-    fill="#5e5c64"
-    viewBox="0 0 36 36"
-    version="1.1"
-    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
+    fill="#5e5c64"
   >
     <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
     <g
@@ -14,28 +12,13 @@ export const AdminIcon: React.FC<React.SVGAttributes<{}>> = (props) => (
       strokeLinejoin="round"
     ></g>
     <g id="SVGRepo_iconCarrier">
-      <title>administrator-line</title>
-      <path
-        fill="currentColor"
-        d="M14.68,14.81a6.76,6.76,0,1,1,6.76-6.75A6.77,6.77,0,0,1,14.68,14.81Zm0-11.51a4.76,4.76,0,1,0,4.76,4.76A4.76,4.76,0,0,0,14.68,3.3Z"
-        className="clr-i-outline clr-i-outline-path-1"
-      ></path>
-      <path
-        fill="currentColor"
-        d="M16.42,31.68A2.14,2.14,0,0,1,15.8,30H4V24.22a14.81,14.81,0,0,1,11.09-4.68l.72,0a2.2,2.2,0,0,1,.62-1.85l.12-.11c-.47,0-1-.06-1.46-.06A16.47,16.47,0,0,0,2.2,23.26a1,1,0,0,0-.2.6V30a2,2,0,0,0,2,2H16.7Z"
-        className="clr-i-outline clr-i-outline-path-2"
-      ></path>
-      <path
-        fill="currentColor"
-        d="M26.87,16.29a.37.37,0,0,1,.15,0,.42.42,0,0,0-.15,0Z"
-        className="clr-i-outline clr-i-outline-path-3"
-      ></path>
-      <path
-        fill="currentColor"
-        d="M33.68,23.32l-2-.61a7.21,7.21,0,0,0-.58-1.41l1-1.86A.38.38,0,0,0,32,19l-1.45-1.45a.36.36,0,0,0-.44-.07l-1.84,1a7.15,7.15,0,0,0-1.43-.61l-.61-2a.36.36,0,0,0-.36-.24H23.82a.36.36,0,0,0-.35.26l-.61,2a7,7,0,0,0-1.44.6l-1.82-1a.35.35,0,0,0-.43.07L17.69,19a.38.38,0,0,0-.06.44l1,1.82A6.77,6.77,0,0,0,18,22.69l-2,.6a.36.36,0,0,0-.26.35v2.05A.35.35,0,0,0,16,26l2,.61a7,7,0,0,0,.6,1.41l-1,1.91a.36.36,0,0,0,.06.43l1.45,1.45a.38.38,0,0,0,.44.07l1.87-1a7.09,7.09,0,0,0,1.4.57l.6,2a.38.38,0,0,0,.35.26h2.05a.37.37,0,0,0,.35-.26l.61-2.05a6.92,6.92,0,0,0,1.38-.57l1.89,1a.36.36,0,0,0,.43-.07L32,30.4A.35.35,0,0,0,32,30l-1-1.88a7,7,0,0,0,.58-1.39l2-.61a.36.36,0,0,0,.26-.35V23.67A.36.36,0,0,0,33.68,23.32ZM24.85,28a3.34,3.34,0,1,1,3.33-3.33A3.34,3.34,0,0,1,24.85,28Z"
-        className="clr-i-outline clr-i-outline-path-4"
-      ></path>
-      <rect x="0" y="0" width="36" height="36" fill-opacity="0"></rect>
+      <g>
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path
+          fill="currentColor"
+          d="M12 14v2a6 6 0 0 0-6 6H4a8 8 0 0 1 8-8zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm0-2c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm9 6h1v5h-8v-5h1v-1a3 3 0 0 1 6 0v1zm-2 0v-1a1 1 0 0 0-2 0v1h2z"
+        ></path>{' '}
+      </g>
     </g>
   </svg>
 );
